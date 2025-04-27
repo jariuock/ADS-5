38ae7d8dc046070b8a20bfdb2c94faac3fe4cb28
jariuock <garanin.019@gmail.com>
2025-04-27 11:39:04 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,139 +3,78 @@
 #include <map>
 #include "tstack.h"
 
-
-int precedence(char op) {
-    static const std::map<char, int> precedenceMap = {
-        {'+', 1},
-        {'-', 1},
-        {'*', 2},
-        {'/', 2}
-    };
-
-    auto it = precedenceMap.find(op);
-    return (it != precedenceMap.end()) ? it->second : 0;
-}
-
-bool isDigit(char ch) {
-    return ch >= '0' && ch <= '9';
+int getPriority(char op) {
+    if (op == '+' || op == '-') return 1;
+    if (op == '*' || op == '/') return 2;
+    return 0;
 }
 
 std::string infx2pstfx(const std::string& inf) {
     TStack<char, 100> stack;
-    std::string result;
-
+    std::string post;
     for (size_t i = 0; i < inf.length(); ++i) {
-        char ch = inf[i];
-
-        if (isDigit(ch)) {
-            while (i < inf.length() && isDigit(inf[i])) {
-                result += inf[i];
-                ++i;
+        if (inf[i] >= '0' && inf[i] <= '9') { 
+            while (i < inf.length() && inf[i] >= '0' && inf[i] <= '9') { 
+                post += inf[i];
+                i++;
             }
-            result += ' '; 
-            --i; 
+            post += ' ';
+            i--;
         }
- 
-        else if (ch == '(') {
-            if (!stack.push(ch)) {
-                return ""; 
-            }
+        else if (inf[i] == '(') {
+            stack.push('(');
         }
-        
-        else if (ch == ')') {
-            while (!stack.isEmpty() && stack.top() != '(') {
-                result += stack.pop();
-                result += ' ';
+        else if (inf[i] == ')') {
+            while (!stack.isEmpty() && stack.peek() != '(') {
+                post += stack.pop();
+                post += ' ';
             }
-            if (stack.isEmpty()) {
-                return ""; 
-            }
-            stack.pop(); 
+            if (!stack.isEmpty())
+                stack.pop();
         }
-
-        else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
-            while (!stack.isEmpty() && precedence(stack.top()) >= precedence(ch)) {
-                result += stack.pop();
-                result += ' ';
+        else if (inf[i] == '+' || inf[i] == '-' || inf[i] == '*' || inf[i] == '/') {
+            while (!stack.isEmpty() && getPriority(stack.peek()) >= getPriority(inf[i])) {
+                post += stack.pop();
+                post += ' ';
             }
-            if (!stack.push(ch)) {
-                return "";
-            }
+            stack.push(inf[i]);
         }
     }
-
     while (!stack.isEmpty()) {
-        result += stack.pop();
-        result += ' ';
+        post += stack.pop();
+        post += ' ';
     }
-
-    if (!result.empty() && result.back() == ' ') {
-        result.pop_back();
-    }
-
-    return result;
-}
-
-void splitString(const std::string& str, std::string tokens[], int& tokenCount) {
-    size_t start = 0;
-    size_t end = str.find(' ');
-    tokenCount = 0;
-
-    while (end != std::string::npos) {
-        tokens[tokenCount++] = str.substr(start, end - start);
-        start = end + 1;
-        end = str.find(' ', start);
-    }
-
-    if (start < str.length()) {
-        tokens[tokenCount++] = str.substr(start);
-    }
+    if (!post.empty() && post.back() == ' ')
+        post.pop_back();
+    return post;
 }
 
 int eval(const std::string& post) {
     TStack<int, 100> stack;
-
-    std::string tokens[100];
-    int tokenCount = 0;
-    splitString(post, tokens, tokenCount);
-
-    for (int i = 0; i < tokenCount; ++i) {
-        const std::string& token = tokens[i];
-
-        if (isDigit(token[0])) {
-            int value = 0;
-            for (char ch : token) {
-                value = value * 10 + (ch - '0');
+    size_t i = 0;
+    while (i < post.length()) {
+        if (post[i] >= '0' && post[i] <= '9') { 
+            int num = 0;
+            while (i < post.length() && post[i] >= '0' && post[i] <= '9') { 
+                num = num * 10 + (post[i] - '0');
+                i++;
             }
-            if (!stack.push(value)) {
-                return -1; 
-            }
+            stack.push(num);
         }
-        else {
-
+        else if (post[i] == '+' || post[i] == '-' || post[i] == '*' || post[i] == '/') {
             int b = stack.pop();
             int a = stack.pop();
-
-            if (token[0] == '+') {
-                stack.push(a + b);
-            }
-            else if (token[0] == '-') {
-                stack.push(a - b);
-            }
-            else if (token[0] == '*') {
-                stack.push(a * b);
-            }
-            else if (token[0] == '/') {
-                if (b == 0) {
-                    return -1; 
-                }
-                stack.push(a / b);
-            }
-            else {
-                return -1; 
-            }
+            int res = 0;
+            if (post[i] == '+') res = a + b;
+            else if (post[i] == '-') res = a - b;
+            else if (post[i] == '*') res = a * b;
+            else if (post[i] == '/') res = a / b;
+            stack.push(res);
+            i++;
+        }
+        else {
+            i++;
         }
     }
-
     return stack.pop();
 }
